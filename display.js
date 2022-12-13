c7ca1163cdb4446fa4c5bcf297cb2f495eafeebb
Joseph Lander <josephclander@gmail.com>
2022-12-13 14:08:50 +0000

    Add Display class for no transactions

---
+++ display.js
@@ -0,0 +1,12 @@
+// file: display.js
+
+/* Class to show account statements at given time */
+class Display {
+  constructor() {}
+
+  displayTransactions() {
+    return "date || credit || debit || balance\n";
+  }
+}
+
+module.exports = Display;
---
+++ display.test.js
@@ -0,0 +1,10 @@
+// file: display.test.js
+const Display = require("./display");
+
+describe("Display", () => {
+  it("display a header row only when there are no transactions in the array", () => {
+    const accountDisplay = new Display([]);
+    const result = "date || credit || debit || balance\n";
+    expect(accountDisplay.displayTransactions()).toEqual(result);
+  });
+});
