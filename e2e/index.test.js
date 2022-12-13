82dc68df6bf87f7e2d6b6c72cfe8696f1218cf24
Joseph Lander <josephclander@gmail.com>
2022-12-13 17:48:34 +0000

    Add e2e test for multiple transactions

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -7,9 +7,21 @@ describe('End to End testing of Application', () => {
     const clientAccount = new Account();
     clientAccount.addDeposit(1000);
     const accountDisplay = new Display(clientAccount.listTransactions());
+    // not testing the date function
     // adding todays date into test
     const todaysDateFormatted = new Date().toLocaleDateString();
     const expected = `date || credit || debit || balance\n${todaysDateFormatted} || 1000.00 || || 1000.00`;
+    expect(accountDisplay.displayTransactions()).toEqual(expected);
+  });
+  it('returns the correct string for multiple transactions', () => {
+    const clientAccount = new Account();
+    clientAccount.addDeposit(1000);
+    clientAccount.addDeposit(2000);
+    clientAccount.addWithdrawal(500);
+    const accountDisplay = new Display(clientAccount.listTransactions());
+    // adding todays date into test for all transactions
+    const todaysDateFormatted = new Date().toLocaleDateString();
+    const expected = `date || credit || debit || balance\n${todaysDateFormatted} || || 500.00 || 2500.00\n${todaysDateFormatted} || 2000.00 || || 3000.00\n${todaysDateFormatted} || 1000.00 || || 1000.00`;
     expect(accountDisplay.displayTransactions()).toEqual(expected);
   });
 });
