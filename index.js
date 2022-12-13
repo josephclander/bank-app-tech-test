db8a7db58b981725e5e0388d181284895ae37b65
Joseph Lander <josephclander@gmail.com>
2022-12-13 17:57:30 +0000

    Add example code in index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // file: index.js
 
-// where files will be imported for integration
+const Account = require('./src/account');
+const Display = require('./src/display');
+
+// running the requirements
+const clientAccount = new Account();
+clientAccount.addDeposit(1000);
+clientAccount.addDeposit(2000);
+clientAccount.addWithdrawal(500);
+const accountDisplay = new Display(clientAccount.listTransactions());
+
+// eslint-disable-next-line no-console
+console.log(accountDisplay.displayTransactions());
